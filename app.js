d58c1730417b8201367f46ff5eb593e45cad6e42
Maximus203 <printf0cherif@gmail.com>
2022-10-26 12:30:39 +0000

    test-02

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.write('<h1>Hello everyone</h1>');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>Bonjour a toute la team meta</h1>');
 });
 
 server.listen(port, hostname, () => {
