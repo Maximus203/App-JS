1683bd6cf627581b483cf887a3c09b06659bb11d
Maximus203 <printf0cherif@gmail.com>
2022-10-28 18:43:42 +0000

    test-04

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const http = require('http');
+const http = require('http')
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h1>Bonjour a toute la team meta</h1>');
-});
+    console.log(req)
+    res.write("Bienvenue tout le monde !")
+    res.end()
+})
 
 server.listen(port, hostname, () => {
     console.log(`Le serveur est lancer sur cette adresse: http://${hostname}:${port}/`);
